28f967d908613595e62289167a9e1ff077b1a18b
Ben Crinion <77635121+BenCrinion-IW@users.noreply.github.com>
2025-01-31 22:48:33 +0000

    Initial commit

---
+++ WordCount.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace WordCount.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // assert true equals false
+        Assert.True(true);
+    }
+}
---
+++ WordCount/Program.cs
@@ -0,0 +1,9 @@
+﻿// parse input parameters
+if (args.Length != 1)
+{
+    Console.WriteLine("Usage: WordCount <filename>");
+    return;
+}
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
