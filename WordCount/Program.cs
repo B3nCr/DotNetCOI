c9d8ed7144d566e726abeebe60700ccefa653f2d
Ben Crinion <77635121+BenCrinion-IW@users.noreply.github.com>
2025-02-02 20:27:44 +0000

    add support for reading from standard input and update ReadFileHandler accordingly. Needs refactoring.

--- WordCount/Program.cs
+++ WordCount/Program.cs
@@ -9,39 +9,47 @@ internal class Program
         var fileOption = new Option<FileInfo>(
             aliases: new[] { "--file", "-f" },
             description: "The file to read and display on the console.");
-        fileOption.IsRequired = true;
+
+        var stdinOption = new Option<bool>(
+            aliases: new[] { "--stdin", "-s" },
+            description: "Read from standard input.");
 
         var lineOption = new Option<bool>(
             aliases: new[] { "--line", "-l" },
             description: "Return the number of lines in the file.");
 
-        // add byte count option
         var byteOption = new Option<bool>(
             aliases: new[] { "--byte", "-b" },
             description: "Return the number of bytes in the file.");
 
-        // add word count option
         var wordOption = new Option<bool>(
             aliases: new[] { "--word", "-w" },
             description: "Return the number of words in the file.");
 
-        // add character count option
         var charOption = new Option<bool>(
             aliases: new[] { "--char", "-c" },
             description: "Return the number of characters in the file.");
 
         var rootCommand = new RootCommand("Console app to read a file. Similar to the WC command.");
         rootCommand.AddOption(fileOption);
+        rootCommand.AddOption(stdinOption);
         rootCommand.AddOption(lineOption);
         rootCommand.AddOption(byteOption);
         rootCommand.AddOption(wordOption);
         rootCommand.AddOption(charOption);
 
-        rootCommand.SetHandler((file, line, byteFlag, wordFlag, charFlag) =>
+        rootCommand.SetHandler((file, stdin, line, byteFlag, wordFlag, charFlag) =>
             {
-                ReadFileHandler.ReadFile(file, line, byteFlag, wordFlag, charFlag);
+                if (stdin)
+                {
+                    ReadFileHandler.ReadFromStdin(line, byteFlag, wordFlag, charFlag);
+                }
+                else
+                {
+                    ReadFileHandler.ReadFile(file, line, byteFlag, wordFlag, charFlag);
+                }
             },
-            fileOption, lineOption, byteOption, wordOption, charOption);
+            fileOption, stdinOption, lineOption, byteOption, wordOption, charOption);
 
         return await rootCommand.InvokeAsync(args);
     }
--- WordCount/ReadFileHandler.cs
+++ WordCount/ReadFileHandler.cs
@@ -4,8 +4,6 @@ public class ReadFileHandler
 {
     public static void ReadFile(FileInfo file, bool line = false, bool byteFlag = false, bool wordFlag = false, bool characterFlag = false)
     {
-        // Console.WriteLine($"line = {line}, byteFlag = {byteFlag}, wordFlag = {wordFlag}, characterFlag = {characterFlag}");
-
         string content = File.ReadAllText(file.FullName);
 
         if (line)
@@ -30,6 +28,40 @@ public class ReadFileHandler
         {
             int charCount = content.Length;
             Console.WriteLine($"File {file.Name} has {charCount} characters.");
+        }
+
+        if (!line && !byteFlag && !wordFlag && !characterFlag)
+        {
+            Console.WriteLine("Done Nothing.");
+        }
+    }
+
+    public static void ReadFromStdin(bool line = false, bool byteFlag = false, bool wordFlag = false, bool characterFlag = false)
+    {
+        string content = Console.In.ReadToEnd();
+
+        if (line)
+        {
+            int lineCount = content.Split('\n').Length;
+            Console.WriteLine($"Input has {lineCount} lines.");
+        }
+
+        if (byteFlag)
+        {
+            long byteCount = System.Text.Encoding.UTF8.GetByteCount(content);
+            Console.WriteLine($"Input has {byteCount} bytes.");
+        }
+
+        if (wordFlag)
+        {
+            int wordCount = content.Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).Length;
+            Console.WriteLine($"Input has {wordCount} words.");
+        }
+
+        if (characterFlag)
+        {
+            int charCount = content.Length;
+            Console.WriteLine($"Input has {charCount} characters.");
         }
 
         if (!line && !byteFlag && !wordFlag && !characterFlag)
