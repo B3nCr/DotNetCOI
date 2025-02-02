fb4ba5f153a0fc1cf3cf8c2e207d7a60ea69a8eb
Ben Crinion <77635121+BenCrinion-IW@users.noreply.github.com>
2025-02-02 20:33:00 +0000

    refactor ReadFileHandler to use more descriptive parameter names for clarity

--- WordCount/ReadFileHandler.cs
+++ WordCount/ReadFileHandler.cs
@@ -2,69 +2,69 @@ using System.IO;
 
 public class ReadFileHandler
 {
-    public static void ReadFile(FileInfo file, bool line = false, bool byteFlag = false, bool wordFlag = false, bool characterFlag = false)
+    public static void ReadFile(FileInfo file, bool countLines = false, bool countBytes = false, bool countWords = false, bool countCharacters = false)
     {
         string content = File.ReadAllText(file.FullName);
 
-        if (line)
+        if (countLines)
         {
             int lineCount = content.Split('\n').Length;
             Console.WriteLine($"File {file.Name} has {lineCount} lines.");
         }
 
-        if (byteFlag)
+        if (countBytes)
         {
             long byteCount = file.Length;
             Console.WriteLine($"File {file.Name} has {byteCount} bytes.");
         }
 
-        if (wordFlag)
+        if (countWords)
         {
             int wordCount = content.Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).Length;
             Console.WriteLine($"File {file.Name} has {wordCount} words.");
         }
 
-        if (characterFlag)
+        if (countCharacters)
         {
             int charCount = content.Length;
             Console.WriteLine($"File {file.Name} has {charCount} characters.");
         }
 
-        if (!line && !byteFlag && !wordFlag && !characterFlag)
+        if (!countLines && !countBytes && !countWords && !countCharacters)
         {
             Console.WriteLine("Done Nothing.");
         }
     }
 
-    public static void ReadFromStdin(bool line = false, bool byteFlag = false, bool wordFlag = false, bool characterFlag = false)
+    public static void ReadFromStdin(bool countLines = false, bool countBytes = false, bool countWords = false, bool countCharacters = false)
     {
         string content = Console.In.ReadToEnd();
 
-        if (line)
+        if (countLines)
         {
             int lineCount = content.Split('\n').Length;
             Console.WriteLine($"Input has {lineCount} lines.");
         }
 
-        if (byteFlag)
+        if (countBytes)
         {
             long byteCount = System.Text.Encoding.UTF8.GetByteCount(content);
             Console.WriteLine($"Input has {byteCount} bytes.");
         }
 
-        if (wordFlag)
+        if (countWords)
         {
             int wordCount = content.Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).Length;
             Console.WriteLine($"Input has {wordCount} words.");
         }
 
-        if (characterFlag)
+        if (countCharacters)
         {
             int charCount = content.Length;
             Console.WriteLine($"Input has {charCount} characters.");
         }
 
-        if (!line && !byteFlag && !wordFlag && !characterFlag)
+        if (!countLines && !countBytes && !countWords && !countCharacters)
         {
             Console.WriteLine("Done Nothing.");
         }
